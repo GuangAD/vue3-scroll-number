8d922e37d90d56933a0a9e8851b456be2d836228
gaoguangce <1536576466@qq.com>
2024-08-12 16:15:27 +0800

    feat: 配置生成d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), dts({})],
+  plugins: [vue(), vueDevTools(), dts({ tsconfigPath: './tsconfig.build.json' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
